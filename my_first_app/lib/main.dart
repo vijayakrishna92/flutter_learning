4b43245604a3189fae9cb0133e69b1b8f9317abc
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2025-05-27 01:22:14 +0530

    add buttons and icons

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,28 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MaterialApp(
-  home: Home(),
-));
+void main() => runApp(
+  MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text("myapp"), backgroundColor: const Color.fromARGB(255, 143, 112, 67)),
+      body: Center(
+        child: IconButton(onPressed: (){}, 
+        icon: Icon(Icons.architecture),
+        color:const Color.fromARGB(255, 64, 201, 255) ,
+        ),
 
-class Home extends StatelessWidget { //cannot change over time.
-  const Home({super.key});
+      )
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-    appBar: AppBar(
-      title: Text('My first app'),
-      titleTextStyle: TextStyle(color: Colors.amber),
-      centerTitle: true,
-      backgroundColor: const Color.fromARGB(204, 228, 12, 12),
     ),
-    body: Center(
-      //child: Image(image: NetworkImage('https://images.pexels.com/photos/32203509/pexels-photo-32203509/free-photo-of-dramatic-night-view-of-the-cn-tower-in-toronto.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')),
-      //child:Image(image: AssetImage('assets/pexels-olegnagovski-32177955.jpg'))//yaml file uncomment assets and enter path of image or folder
-      //child: Image.asset('assets/pexels-naoxotl-32203509.jpg'), //use asset image
-      child: Image.network('https://images.pexels.com/photos/32203509/pexels-photo-32203509/free-photo-of-dramatic-night-view-of-the-cn-tower-in-toronto.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'),
-    )
-  );
-  }
-}
-//pexels-naoxotl-32203509.jpg pexels-olegnagovski-32177955.jpg
+  ),
+);
