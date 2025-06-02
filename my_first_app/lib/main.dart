7b0bd3464a982823c373a27d263d5ea90636e520
vijayakrishna92 <vijaykrishnavk92@gmail.com>
2025-06-03 00:39:49 +0530

    worldclock 1

--- my_first_app/lib/main.dart
+++ my_first_app/lib/main.dart
@@ -1,49 +1,13 @@
 import 'package:flutter/material.dart';
-import 'snippets/quotes.dart';
-import 'snippets/quote_card.dart';
+import 'package:my_first_app/home.dart';
+import 'package:my_first_app/choose_location.dart';
+import 'package:my_first_app/loading.dart';
 
-
-void main() => runApp(MaterialApp(home: QuoteList()));
-
-class QuoteList extends StatefulWidget {
-  const QuoteList({super.key});
-
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
-
-class _QuoteListState extends State<QuoteList> {
-  List<Quotes> quotes = [
-    Quotes(text: 'i am learning flutter', author: 'vijay'),
-    Quotes(text: 'Consistency is key', author: 'Unknown'),
-    Quotes(text: 'Code. Debug. Repeat.', author: 'A Developer'),
-  ];
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.grey[200],
-      appBar: AppBar(
-        title: Text('quotes'),
-        centerTitle: true,
-        backgroundColor: Colors.redAccent,
-      ),
-      body: Column(
-        //children: quotes.map((quote) => QuoteCard(quote: quote)).toList(),
-        children:
-            quotes
-                .map(
-                  (quote) => QuoteCard(
-                    quote: quote,
-                    delete: () {
-                      setState(() {
-                        quotes.remove(quote);
-                      });
-                    },
-                  ),
-                )
-                .toList(),
-      ),
-    );
-  }
-}
+void main() => runApp(MaterialApp(
+  initialRoute: '/home',
+  routes: {
+    '/':(context) => Loading(),
+    '/home':(context) => Home(),
+    '/location': (context) => ChooseLocation(),
+  },
+));
